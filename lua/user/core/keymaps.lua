285d458521ac088d7a62a5b348e69cb7610b0b80
wildanam <ahmadwildan.am@gmail.com>
2024-04-04 10:45:27 +0700

    remove: linting

--- lua/user/core/keymaps.lua
+++ lua/user/core/keymaps.lua
@@ -35,6 +35,9 @@ keymap("n", "<C-Right>", ":vertical resize +2<CR>", opts)
 keymap("n", "<S-l>", ":bnext<CR>", opts)
 keymap("n", "<S-h>", ":bprevious<CR>", opts)
 
+-- Move cursor to the end of the line
+keymap("n", "<leader>el", "$", opts)
+
 -- Insert --
 -- Press jk fast to enter
 keymap("i", "jk", "<ESC>", opts)
